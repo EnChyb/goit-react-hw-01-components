ae3ae7f5fd0955f5a55aa34b909f36d6c6e02b40
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-04-04 23:00:42 +0200

    fixing rename file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-import { Profile } from "./profile/Profile";
+//import { Profile } from "./profile/Profile";
 import user from "./data/user.json"
 
-export const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
-  );
-};
+
+//export const App = () => {
+//  return (
+//    <div>
+//      <Profile
+//        username={user.username}
+ //       tag={user.tag}
+//        location={user.location}
+//        avatar={user.avatar}
+//        stats={user.stats}
+//      />
+ //   </div>
+//  );
+//};
